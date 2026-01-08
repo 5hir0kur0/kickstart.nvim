b9a617c9592d95aa0bac1517e9bafa09f25a08aa
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2026-01-08 11:17:59 +0900

    Disable text objects clashing with default bindings

--- lua/custom/plugins/nvim-various-textobjs.lua
+++ lua/custom/plugins/nvim-various-textobjs.lua
@@ -4,6 +4,8 @@ return {
   opts = {
     keymaps = {
       useDefaults = true,
+      -- Disable restOfIndentation and restOfParagraph textobjects because they clash with native keybindings
+      disabledDefaults = {"r", "R"},
     },
   },
 }
