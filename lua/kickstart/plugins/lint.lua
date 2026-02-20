89f1f51d05f0305032934a4689ee0a8dce53a093
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2026-02-20 19:21:43 +0900

    Replace markdownlint with vale

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -12,7 +12,7 @@ return {
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       lint.linters_by_ft = lint.linters_by_ft or {}
-      lint.linters_by_ft['markdown'] = { 'markdownlint' }
+      lint.linters_by_ft['markdown'] = { 'vale' }
       lint.linters_by_ft['sh'] = { 'shellcheck' }
       lint.linters_by_ft['rust'] = { 'clippy' }
       --
